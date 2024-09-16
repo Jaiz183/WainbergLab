97f419a82b5a151c8487e61a1b03dcd5f559a9ed
Jaiz183 <jaizepic@gmail.com>
2024-09-15 22:04:33 -0400

    Trial commit.

---
+++ ad_prs_scoring.py
@@ -0,0 +1,11 @@
+from utils import run
+from constants import *
+
+PROJECT_DIR = 'projects/def-wainberg'
+
+run(f'{PLINK2} '
+    f'--bfile {PROJECT_DIR}/jeesonja/cleaned_genome/'
+    f'{PLINK_OUTPUT_FILE}_merged_qc_hetero '
+    f'--score {PROJECT_DIR}/jeesonja/scores/prs_weights_without_apoe.txt 1 4 6 '
+    f'header '
+    f'list-variants')
